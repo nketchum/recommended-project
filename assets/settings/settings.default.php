1012eddb9b4a727e8ca95ad61bc08096f885983d
Nicholas Ketchum <nketchum@gmail.com>
2023-01-18 16:36:43 -0500

    rename settings and services files

--- assets/settings/settings.default.php
+++ assets/settings/settings.default.php
@@ -9,9 +9,10 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 $settings['entity_update_backup'] = TRUE;
 $settings['entity_update_batch_size'] = 50;
 $settings['file_scan_ignore_directories'] = ['node_modules', 'bower_components'];
-$settings['hash_salt'] = 'DEVELOPMENT';
+$settings['hash_salt'] = '';
 $settings['maintenance_theme'] = 'claro';
 $settings['migrate_node_migrate_type_classic'] = FALSE;
+$settings['trusted_host_patterns'] = ['.*'];
 $settings['update_free_access'] = FALSE;
 
 // Production settings. Overrides default.
